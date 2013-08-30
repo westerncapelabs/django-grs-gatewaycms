4c9b7c47e448aa85f97275d57daebfa6347d66de
Mark Gituma <gitumarkk@gmail.com>
2013-08-30 11:08:40 +0200

    wrote the api functions

---
+++ quiz/api.py
@@ -0,0 +1,41 @@
+from tastypie.resources import ModelResource
+from tastypie import fields
+from quiz.models import (Quiz, Question, Answer, FinalResponse)
+
+
+
+class QuizResource(ModelResource):
+    q_quiz_id = fields.ToManyField("quiz.api.QuestionResource",
+                                   'q_quiz_id',
+                                    full=True)
+
+    fr_quiz_id = fields.ToManyField("quiz.api.FinalResponseResource",
+                                   'fr_quiz_id',
+                                    full=True)
+    class Meta:
+        queryset = Quiz.objects.all()
+        resource_name = "quiz"
+        list_allowed_methods = ['post', 'get'] 
+        include_resource_uri = True
+        always_return_data = True
+
+
+class QuestionResource(ModelResource):
+    question_id = fields.ToManyField("quiz.api.AnswerResource",
+                                  'question_id',
+                                  full=True)
+
+    class Meta:
+        queryset = Question.objects.all()
+
+
+class AnswerResource(ModelResource):
+    class Meta:
+        queryset = Answer.objects.all()
+        include_resource_uri = False
+
+
+class FinalResponseResource(ModelResource):
+    class Meta:
+        queryset = FinalResponse.objects.all()
+        include_resource_uri = False
