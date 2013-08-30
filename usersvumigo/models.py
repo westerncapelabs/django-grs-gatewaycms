075d54a1745601637d964e3011dd91d49707b705
Mark Gituma <gitumarkk@gmail.com>
2013-08-30 13:04:33 +0200

    made users model

--- usersvumigo/models.py
+++ usersvumigo/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class VumiGoUser(models.Model):
+    msisdn = models.CharField(max_length=30,
+                              blank=False,
+                              verbose_name="MSISDN (Mobile Number)")
+    sex = models.CharField(max_length=6)
+    age = models.CharField(max_length=30)
+    community = models.CharField(max_length=30)
+
+    def __unicode__(self):
+        return self.msisdn
+
+    class Meta:
+        verbose_name = "User"
