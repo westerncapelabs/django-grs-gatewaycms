e61b944b7e629c54d44750c85bd3ab3463158f1c
Mark Gituma <gitumarkk@gmail.com>
2013-08-30 13:15:33 +0200

    schemamigration of the users model

---
+++ usersvumigo/migrations/0001_initial.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'VumiGoUser'
+        db.create_table(u'usersvumigo_vumigouser', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('msisdn', self.gf('django.db.models.fields.CharField')(max_length=30)),
+            ('sex', self.gf('django.db.models.fields.CharField')(max_length=6)),
+            ('age', self.gf('django.db.models.fields.CharField')(max_length=30)),
+            ('community', self.gf('django.db.models.fields.CharField')(max_length=30)),
+        ))
+        db.send_create_signal(u'usersvumigo', ['VumiGoUser'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'VumiGoUser'
+        db.delete_table(u'usersvumigo_vumigouser')
+
+
+    models = {
+        u'usersvumigo.vumigouser': {
+            'Meta': {'object_name': 'VumiGoUser'},
+            'age': ('django.db.models.fields.CharField', [], {'max_length': '30'}),
+            'community': ('django.db.models.fields.CharField', [], {'max_length': '30'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'msisdn': ('django.db.models.fields.CharField', [], {'max_length': '30'}),
+            'sex': ('django.db.models.fields.CharField', [], {'max_length': '6'})
+        }
+    }
+
+    complete_apps = ['usersvumigo']
