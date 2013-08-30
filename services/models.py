862db2ad5b9f66067d67b0da0aecac218459d380
Mark Gituma <gitumarkk@gmail.com>
2013-08-30 14:45:20 +0200

    added the Service model

--- services/models.py
+++ services/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Service(models.Model):
+    name = models.CharField(max_length=125, blank=False)
+    content_1 = models.CharField(max_length=125, blank=True)
+    content_2 = models.CharField(max_length=125, blank=True)
+    content_3 = models.CharField(max_length=125, blank=True)
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Service"
