b07335f6c47c7057f04e17355c65a256972d5b65
Mark Gituma <gitumarkk@gmail.com>
2013-09-02 10:21:36 +0200

    added created at

--- usersvumigo/admin.py
+++ usersvumigo/admin.py
@@ -3,7 +3,7 @@ from usersvumigo.models import (VumiGoUser)
 
 
 class VumiGoUserAdmin(admin.ModelAdmin):
-    list_display = ["msisdn", "sex", "age", "community"]
+    list_display = ["msisdn", "sex", "age", "community", "created_at"]
 
 
 admin.site.register(VumiGoUser, VumiGoUserAdmin)
