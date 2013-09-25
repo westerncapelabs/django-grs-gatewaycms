ff85cc1484dead024bbfeee4ed13acd83a112f66
gitumarkk <gitumarkk@gmail.com>
2013-09-25 17:35:01 +0200

    removed print

--- usersvumigo/tasks.py
+++ usersvumigo/tasks.py
@@ -6,7 +6,14 @@ import json
 
 @task
 def send_bulk_sms(bulk_sms):
-    print "sending bulk sms"
+    """
+    What if SMS is not sent?
+    """
     data = {'objects' : bulk_sms}
     headers = {"content-type": "application/json"}
-    response = requests.patch(settings.GOPHER_BASE_URL, data=json.dumps(data), headers=headers)
+    url = "%s?username=%s&api_key=%s" % (settings.GOPHER_BASE_URL,
+                                         settings.GOPHER_USERNAME,
+                                         settings.GOPHER_API_KEY)
+    response = requests.patch(url,
+                              data=json.dumps(data),
+                              headers=headers)
