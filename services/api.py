ec8e29ee671f6ad9e956b1c68d6ba5cd44e140c8
Mike Jones <mike@westerncapelabs.com>
2013-10-08 13:26:33 +0200

    Fix Services API

--- services/api.py
+++ services/api.py
@@ -4,28 +4,17 @@ from services.models import Service, Category
 from tastypie.authorization import Authorization
 
 
-# class ServiceResource(ModelResource):
-#     class Meta:
-#         queryset = Category.objects.all()
-#         resource_name = "services"
-#         list_allowed_methods = ['get'] 
-#         include_resource_uri = True
-#         always_return_data = True
-#         authorization = Authorization()
-
-
-
 class CategoryResource(ModelResource):
-    services = fields.ToManyField("services.api.ServiceResource",
-                                   'services', full=True)
-
+    category = fields.ToManyField("services.api.ServiceResource",
+                                   'category', full=True)
 
     class Meta:
         queryset = Category.objects.all()
         resource_name = "category"
-        list_allowed_methods = ['get']
+        list_allowed_methods = ['get'] 
         include_resource_uri = True
         always_return_data = True
+        # authorization = Authorization()
 
     def get_object_list(self, request):
         query = super(CategoryResource, self).get_object_list(request)
