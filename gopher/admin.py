8613d439b9e2c128f83b918ec8adb3476661da70
gitumarkk <gitumarkk@gmail.com>
2013-10-08 18:13:51 +0200

    registered models with admin

---
+++ gopher/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from gopher.models import AirtimeApplication, SendAirtime
+
+
+class AirtimeApplicationAdmin(admin.ModelAdmin):
+    list_display = ["name", "ratio", "per_day", "amount", "active", "product_key"]
+
+
+class SendAirtimeAdmin(admin.ModelAdmin):
+    list_display = ["app_id", "msisdn", "product_key", "amount", "sent", "created_at"]
+
+
+admin.site.register(AirtimeApplication, AirtimeApplicationAdmin)
+admin.site.register(SendAirtime, SendAirtimeAdmin)
