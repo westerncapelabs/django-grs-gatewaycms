7dcad2598471780743ecccca79fc3b9b347ed348
gitumarkk <gitumarkk@gmail.com>
2013-10-08 18:13:43 +0200

    added required models

--- gopher/models.py
+++ gopher/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class AirtimeApplication(models.Model):
+    name = models.CharField(max_length=6)
+    ratio  = models.IntegerField(null=True)
+    per_day  = models.IntegerField(null=True)
+    amount  = models.IntegerField(null=True)
+    active = models.BooleanField()
+    product_key = models.CharField(max_length=10)
+
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Airtime Application"
+
+class SendAirtime(models.Model):
+    app_id = models.ForeignKey(AirtimeApplication)
+    msisdn = models.CharField(max_length=30,
+                              verbose_name="MSISDN (Mobile Number)")
+    product_key = models.CharField(max_length=10)
+    amount  = models.IntegerField()
+    sent = models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True,
+                                      blank=False,
+                                      editable=False)
+
+    def __unicode__(self):
+        return self.msisdn
+
+    class Meta:
+        verbose_name = "Send Airtime"
