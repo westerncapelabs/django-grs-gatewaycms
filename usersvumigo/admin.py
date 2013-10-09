c6e0239d75712b0444ca2df5ae88e7c26b37d5af
Mike Jones <mike@westerncapelabs.com>
2013-10-09 23:14:26 +0200

    Make it DRY

--- usersvumigo/admin.py
+++ usersvumigo/admin.py
@@ -3,7 +3,7 @@ from usersvumigo.models import (VumiGoUser, QuizResponse)
 from gopher.models import SendAirtime, AirtimeApplication
 
 
-def send_five_rand_airtime_to_selected(modeladmin, request, queryset):
+def send_airtime_to_selected(modeladmin, request, queryset, amount):
     """
     Post Structure:
     {
@@ -15,47 +15,23 @@ def send_five_rand_airtime_to_selected(modeladmin, request, queryset):
     }
     """
     app = AirtimeApplication.objects.get(name="Registered User Airtime")
-    amount = 500  # cents
+    amount = amount  # cents
     product_key = "AIRTIME"
     create_list = [SendAirtime(app_id_id=app.id, msisdn=obj.msisdn,
         amount=amount, product_key=product_key) for obj in queryset]
     SendAirtime.objects.bulk_create(create_list)
 
 
-def send_ten_rand_airtime_to_selected(modeladmin, request, queryset):
-    """
-    Post Structure:
-    {
-        "denomination": 1000,
-        "product_code": "AIRTIME",
-        "notes": "Grassroots Random Winner",
-        "msisdn": 27821231231,
-        "project_id": ""
-    }
-    """
-    app = AirtimeApplication.objects.get(name="Registered User Airtime")
-    amount = 1000  # cents
-    product_key = "AIRTIME"
-    create_list = [SendAirtime(app_id_id=app.id, msisdn=obj.msisdn, amount=amount, product_key=product_key) for obj in queryset]
-    SendAirtime.objects.bulk_create(create_list)
+def send_five_rand_airtime_to_selected(modeladmin, request, queryset):
+    send_airtime_to_selected(modeladmin, request, queryset, 500)
 
+
+def send_ten_rand_airtime_to_selected(modeladmin, request, queryset):
+    send_airtime_to_selected(modeladmin, request, queryset, 1000)
+    
 
 def send_fifteen_rand_airtime_to_selected(modeladmin, request, queryset):
-    """
-    Post Structure:
-    {
-        "denomination": 10,
-        "product_code": "AIRTIME",
-        "notes": "Grassroots Random Winner",
-        "msisdn": 27821231231,
-        "project_id": ""
-    }
-    """
-    app = AirtimeApplication.objects.get(name="Registered User Airtime")
-    amount = 1500  # cents
-    product_key = "AIRTIME"
-    create_list = [SendAirtime(app_id_id=app.id, msisdn=obj.msisdn, amount=amount, product_key=product_key) for obj in queryset]
-    SendAirtime.objects.bulk_create(create_list)
+    send_airtime_to_selected(modeladmin, request, queryset, 1500)
 
 
 send_five_rand_airtime_to_selected.short_description = "Send R 5 airtime to following users"
