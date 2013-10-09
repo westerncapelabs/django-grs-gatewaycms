b4eb70b8ddda54fe3efa13a8b837ef49a8066af2
gitumarkk <gitumarkk@gmail.com>
2013-10-09 13:21:22 +0200

    added a catch on exception error

--- gopher/tasks.py
+++ gopher/tasks.py
@@ -1,6 +1,7 @@
 from gopher.models import SendAirtime
 from celery.decorators import task
 from celery.utils.log import get_task_logger
+from celery.exceptions import MaxRetriesExceededError
 from django.conf import settings
 import requests
 import json
@@ -10,17 +11,17 @@ logger = get_task_logger(__name__)
 
 @task
 def get_new_recharge():
-    print "Running celery beat"
     logger.info("Checking for new recharges")
     queryset = SendAirtime.objects.filter(sent=False)
     bulk_sms = [{"msisdn": obj.msisdn, "denomination": obj.amount, "product_code": obj.product_key} for obj in queryset.all()]
+    ids = [obj.id for obj in queryset.all()]
     if bulk_sms:
-        send_bulk_sms.delay(bulk_sms)
+        send_bulk_sms.delay(bulk_sms, ids)
         queryset.update(sent=True)
 
 
 @task
-def send_bulk_sms(bulk_sms):
+def send_bulk_sms(bulk_sms, ids):
     """
     What if SMS is not sent?
     """
@@ -30,6 +31,11 @@ def send_bulk_sms(bulk_sms):
     url = "%s?username=%s&api_key=%s" % (settings.GOPHER_BASE_URL,
                                          settings.GOPHER_USERNAME,
                                          settings.GOPHER_API_KEY)
-    response = requests.patch(url,
-                              data=json.dumps(data),
-                              headers=headers)
+    try:
+        response = requests.patch(url,
+                                  data=json.dumps(data),
+                                  headers=headers)
+    except requests.exceptions.ConnectionError:
+        logger.error("There is a connection error so airtime recharge not sent")
+        queryset = SendAirtime.objects.filter(pk__in=ids)
+        queryset.update(sent=False)
