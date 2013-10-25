ab20ae667c217ff6846c9d251014915efac2d174
gitumarkk <gitumarkk@gmail.com>
2013-10-25 17:30:12 +0200

    registered Api with urls

---
+++ gopher/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import patterns, url, include
+from tastypie.api import Api
+from gopher.api import RequestAirtimeSendResource
+
+
+api_resources = Api(api_name='v1')
+api_resources.register(RequestAirtimeSendResource())
+api_resources.prepend_urls()
+
+# Setting the urlpatterns to hook into the api urls
+urlpatterns = patterns('',
+    url(r'^api/', include(api_resources.urls))
+)
