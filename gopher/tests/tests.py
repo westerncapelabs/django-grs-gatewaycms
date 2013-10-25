7c973802e02523f36ef6bf8ad70991045ab48dd1
gitumarkk <gitumarkk@gmail.com>
2013-10-25 17:31:25 +0200

    added unittests skeleton

--- gopher/tests/tests.py
+++ gopher/tests/tests.py
@@ -1,16 +1,14 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
-from django.test import TestCase
+from gopher.models import AirtimeApplication, SendAirtime, RequestAirtimeSend
+from django.core.urlresolvers import reverse
+from tastypie.test import ResourceTestCase
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class TestRequestRandom(ResourceTestCase):
+    def test_post_save_signal(self):
+        pass
+
+    def test_api_sends(self):
+        pass
+
+    def test_random_airtime_added_on_api(self):
+        pass
