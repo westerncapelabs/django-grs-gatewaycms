9aeb0c80f1f5f98409b19741b82ea0a41a542b86
gitumarkk <gitumarkk@gmail.com>
2013-10-25 17:27:24 +0200

    created new request model

--- gopher/models.py
+++ gopher/models.py
@@ -1,3 +1,5 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.db import models
 
 
@@ -32,3 +34,29 @@ class SendAirtime(models.Model):
 
     class Meta:
         verbose_name_plural = "Send Airtime"
+
+
+class RequestAirtimeSend(models.Model):
+    request_application = models.ForeignKey(AirtimeApplication)
+    request_send_airtime = models.ForeignKey(SendAirtime,
+                                             null=True,
+                                             blank=True)
+    msisdn = models.CharField(max_length=30,
+                              verbose_name="MSISDN (Mobile Number)")
+    product_key = models.CharField(max_length=10)
+    amount  = models.IntegerField()
+    sent = models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True,
+                                      blank=False,
+                                      editable=False)
+
+    def __unicode__(self):
+        return self.msisdn
+
+    class Meta:
+        verbose_name_plural = "Request Airtime Send"
+
+
+@receiver(post_save, sender=RequestAirtimeSend)
+def send_random_airtime(sender, instance, signal, created, **kwargs):
+    pass
